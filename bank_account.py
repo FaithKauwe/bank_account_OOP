e8b7224fe17696c21ad177aa414afd6ae089a037
FaithKauwe <fkauwe@gmail.com>
2024-10-26 15:41:46 -0700

    class attributes and methods

--- bank_account.py
+++ bank_account.py
@@ -1,28 +1,61 @@
-# Define a BankAccount class.
-# A bank account should have the following attributes:
 
-#     full_name - the full name of the bank account account owner.
-#     account_number - randomly generated 8 digit number, unique per account.
-#     balance - the balance of money in the account, should start at 0.
 
+import random
 class BankAccount:
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name, account_number=None, balance=0):
         self.full_name = full_name
-        self.account_number = account_number
-        self.balnce = balance
+    # set account number to random 8 digit number    
+        if account_number is None:
+            self.account_number = self.generate_account_number()
+        else:
+            self.account_number = account_number
+        self.balance = balance
+    # function that returns account number
+    def generate_account_number(self):
+        # Generate a random 8-digit number
+        return random.randint(10000000, 99999999)
 
+    def deposit(self, deposit_amount):
+        self.balance = self.balance + deposit_amount
+        print((f"Amount deposited: {deposit_amount}\n"
+              f"New Balance: {self.balance}"))
+        return self.balance
 
-# Then define the following methods for the BankAccount class:
+    def withdraw(self, withdrawal_amount):
+        if self.balance < withdrawal_amount:
+            print((f"Insufficient funds. $10 overdrafte fee assessed. \n"
+                   f"New balance: {self.balance - 10}"))
+    # reassign balance to reflect 10 overdraft
+            self.balance = self.balance - 10
+        else:
+            self.balance = self.balance - withdrawal_amount
+            print((f"Amount withdrawn: {withdrawal_amount} \n"
+              f"New Balance: {self.balance}"))
+        return self.balance
+ 
+    def get_balance(self):
+        print(f"Your current balance is: {self.balance}")
 
-#     The deposit method will take one parameter amount and will add amount to the balance. Also, it will print the message: “Amount deposited: $X.XX new balance: $Y.YY”
-#     The withdraw method will take one parameter amount and will subtract amount from the balance. Also, it will print a message, like “Amount withdrawn: $X.XX new balance: $Y.YY”. If the user tries to withdraw an amount that is greater than the current balance, print ”Insufficient funds.” and charge them with an overdraft fee of $10
-#     The get_balance method will print a user-friendly message with the account balance and then also return the current balance of the account.
-#     The add_interest method adds interest to the users balance. The annual interest rate is 1% (i.e. 0.083% per month). Thus, the monthly interest is calculated by the following equation: interest = balance *  0.00083 .
 #     The print_statement method prints a message with the account name, account number, and balance like this:
-
 # Joi Anderson
 # Account No.: ****5678
 # Balance: $100.00
+    def print_statement(self):
+        # Convert account number to string and format with asterisks
+        account_str = str(self.account_number)
+         # Get last 4 digits
+        masked_account = '****' + account_str[-4:] 
+
+        print((f"{self.full_name} \n"
+         f"Account Number.: {masked_account} \n"
+         f" Balance: {self.balance} "))
+# need to format balance when it prints and maybe set as float?
+
+# Then define the following methods for the BankAccount class:
+
+#     The add_interest method adds interest to the users balance. The annual interest rate is 1% (i.e. 0.083% per month). Thus, the monthly interest is calculated by the following equation: interest = balance *  0.00083 .
+
+
 
 # Outside of the BankAccount class, define 3 different bank account examples using the BankAccount() object.
 
@@ -37,4 +70,8 @@ class BankAccount:
 #     Print a statement
 #     Make a withdrawl of $150 (Mitchell needs some Yeezy's)
 #     Print a statement
+
+mitchell = BankAccount("Mitchell", "03141592")
+mitchell.deposit(400000)
+mitchell.print_statement()
 
